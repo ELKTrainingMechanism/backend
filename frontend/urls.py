fb9e3d515daa75dccdc98737126b42179fb05eb3
Sharat Jacob <sharatjacob2@gmail.com>
2023-05-31 18:56:31 +0530

    testing

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('endpoint/', views.MyEndpoint.as_view(), name='my_endpoint'),
     path('get_csrf_token/', views.get_csrf_token)
-    # Additional API endpoints for your app
 ]
 
